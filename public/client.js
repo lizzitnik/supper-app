62b1a1b39bb91cd8cceae2105eba3f1cb2c0a3e9
Liz Zitnik <lizzitnik4@gmail.com>
2018-02-04 21:10:52 -0800

    added mock data

--- public/client.js
+++ public/client.js
@@ -1,80 +1,162 @@
-var map;
-var service;
-var infowindow;
+// var map;
+// var service;
+// var infowindow;
 
-function geoLocationSuccess(pos) {
-  var crd = pos.coords;
+// var MOCK_GATHERINGS {
+//   'gatherings': [
+//     {
+//       'id': '111',
+//       'host': 'John Smith',
+//       'numberAttending': '5',
+//       'restaurant': 'Joe\'s Crab Shack'
+//     },
+//     {
+//       'id': '222',
+//       'host': 'Jane Smithers',
+//       'numberAttending': '2',
+//       'restaurant': 'McDonalds'
+//     },
+//     {
+//       'id': '333',
+//       'host': 'Will Beans',
+//       'numberAttending': '10',
+//       'restaurant': 'KFC'
+//     },
+//     {
+//       'id': '444',
+//       'host': 'Tom Hardy',
+//       'numberAttending': '4',
+//       'restaurant': 'Starbucks'
+//     }
+//   ]
+// };
 
-  var location = {lat: crd.latitude, lng: crd.longitude};
+// function getRecentGatherings(callbackFn) {
+//     setTimeout(function(){ callbackFn(MOCK_GATHERINGS)}, 100);
+// }
 
-  searcher(crd.latitude, crd.longitude);
+// function displayGatherings(data) {
+//   for (index in data.gatherings) {
+//     $('.search-page').append('<p>' + data.gatherings[index].text + '</p>');
+//   }
+// }
 
-  // var map = new google.maps.Map(document.getElementById('map'), {
-  //         zoom: 8,
-  //         center: location
-  //  });
+// function getAndDisplayGatherings() {
+//   getRecentGatherings(displayGatherings);
+// }
 
-  // var marker = new google.maps.Marker({
-  //         position: location,
-  //         map: map
-  //       });
+// $(function() {
+//   getAndDisplayGatherings();
+// })
 
-  // console.log('Your current position is:');
-  // console.log(`Latitude : ${crd.latitude}`);
-  // console.log(`Longitude: ${crd.longitude}`);
-  // console.log(`More or less ${crd.accuracy} meters.`);
-};
 
-function geoLocationError(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-};
-var options = {
-  enableHighAccuracy: true,
-  maximumAge: 0
-};
-navigator.geolocation.getCurrentPosition(geoLocationSuccess, geoLocationError, options);
-
-function searcher(lat, long) {
-  var pyrmont = new google.maps.LatLng(lat, long);
-
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
-      zoom: 10
-    });
-
-  var request = {
-    location: pyrmont,
-    radius: '50000',
-    type: ['restaurant']
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, searcherSuccess);
+function loginSubmit() {
+  console.log('running login')
+  $('#login-button').on('click', function(e) {
+    console.log('running submit');
+    e.preventDefault();
+    $('.login-page').hide();
+    $('.signup-page').hide();
+    $('.search-create-page').show();
+  });
 }
 
-function searcherSuccess(results, status) {
-  console.log(results);
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(results[i]);
+$(loginSubmit);
 
-    }
-  }
-}
 
-function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
 
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(place.name);
-          infowindow.open(map, this);
-        });
-      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function geoLocationSuccess(pos) {
+//   var crd = pos.coords;
+
+//   var location = {lat: crd.latitude, lng: crd.longitude};
+
+//   searcher(crd.latitude, crd.longitude);
+
+//   // var map = new google.maps.Map(document.getElementById('map'), {
+//   //         zoom: 8,
+//   //         center: location
+//   //  });
+
+//   // var marker = new google.maps.Marker({
+//   //         position: location,
+//   //         map: map
+//   //       });
+
+//   // console.log('Your current position is:');
+//   // console.log(`Latitude : ${crd.latitude}`);
+//   // console.log(`Longitude: ${crd.longitude}`);
+//   // console.log(`More or less ${crd.accuracy} meters.`);
+// };
+
+// function geoLocationError(err) {
+//   console.warn(`ERROR(${err.code}): ${err.message}`);
+// };
+// var options = {
+//   enableHighAccuracy: true,
+//   maximumAge: 0
+// };
+// navigator.geolocation.getCurrentPosition(geoLocationSuccess, geoLocationError, options);
+
+// function searcher(lat, long) {
+//   var pyrmont = new google.maps.LatLng(lat, long);
+
+//   map = new google.maps.Map(document.getElementById('map'), {
+//       center: pyrmont,
+//       zoom: 10
+//     });
+
+//   var request = {
+//     location: pyrmont,
+//     radius: '50000',
+//     type: ['restaurant']
+//   };
+
+//   service = new google.maps.places.PlacesService(map);
+//   service.nearbySearch(request, searcherSuccess);
+// }
+
+// function searcherSuccess(results, status) {
+//   console.log(results);
+//   if (status == google.maps.places.PlacesServiceStatus.OK) {
+//     for (var i = 0; i < results.length; i++) {
+//       var place = results[i];
+//       createMarker(results[i]);
+
+//     }
+//   }
+// }
+
+// function createMarker(place) {
+//         var placeLoc = place.geometry.location;
+//         var marker = new google.maps.Marker({
+//           map: map,
+//           position: place.geometry.location
+//         });
+
+//         google.maps.event.addListener(marker, 'click', function() {
+//           infowindow.setContent(place.name);
+//           infowindow.open(map, this);
+//         });
+//       }
 
 
 

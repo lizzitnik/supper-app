d78286ed624f1283daa60587308805f626416dad
Liz Zitnik <lizzitnik4@gmail.com>
2018-02-05 15:04:49 -0800

    moved code around

--- supperRouter.js
+++ supperRouter.js
@@ -4,4 +4,24 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
-const {Gathering} = require()
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config');
+const {Gathering} = require('./models');
+
+app.get('/gatherings', (req, res) => {
+	Gathering
+		.find()
+		.limit(10)
+		.then(gatherings => {
+			res.json({
+				gatherings: gatherings.map(
+					(gathering) => gathering.serialize())
+			});
+		})
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({message: 'Internal server error'});
+		});
+});

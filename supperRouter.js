2dd21ca4ce5ed805567903a30dba3c3b9aeac343
Liz Zitnik <lizzitnik4@gmail.com>
2018-02-07 14:43:11 -0800

    added html and css and finished router

--- supperRouter.js
+++ supperRouter.js
@@ -25,6 +25,16 @@ router.get('/', (req, res) => {
 		});
 });
 
+router.get('/:id', (req, res) => {
+  Gathering
+    .findById(req.params.id)
+    .then(post => res.json(post.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'something went horribly awry' });
+    });
+});
+
 router.use(bodyParser.json());
 
 router.post('/', (req, res) => {
@@ -51,5 +61,54 @@ console.log(req.body);
     });
 
 });
+
+router.delete('/:id', (req, res) => {
+  Gathering
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).json({ message: 'success' });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'something went terribly wrong' });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    res.status(400).json({
+      error: 'Request path id and request body id values must match'
+    });
+  }
+
+  const updated = {};
+  const updateableFields = ['host', 'date', 'restaurant'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updated[field] = req.body[field];
+    }
+  });
+
+  Gathering
+    .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
+    .then(updatedPost => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Something went wrong' }));
+});
+
+router.delete('/:id', (req, res) => {
+  Gathering
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+      console.log(`Deleted blog post with id \`${req.params.id}\``);
+      res.status(204).end();
+    });
+});
+
+
+router.use('*', function (req, res) {
+  res.status(404).json({ message: 'Not Found' });
+});
+
+
 
 module.exports = router;
